af3f1e05b769048ccf9d0071501bd956cab4d7b3
DataFire <info@datafire.io>
2020-06-23 01:53:36 -0500

    Edit DataFire project

--- actions/action.js
+++ actions/action.js
@@ -3,6 +3,10 @@ let datafire = require('datafire');
 
 let apache_qakka = require('@datafire/apache_qakka').actions;
 module.exports = new datafire.Action({
+  inputs: [{
+    type: "string",
+    title: "input1"
+  }],
   handler: async (input, context) => {
     let apiResponse = await Promise.all([].map(item => apache_qakka.sendMessageBinary({
       queueName: "",
