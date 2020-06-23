8c62ff77f8d01a29a21edf250d9cb6f31c906748
DataFire <info@datafire.io>
2020-06-23 02:09:24 -0500

    Edit DataFire project

--- actions/action4.js
+++ actions/action4.js
@@ -1,15 +1,8 @@
 "use strict";
 let datafire = require('datafire');
 
-let box_authorization = require('@datafire/box_authorization').actions;
 module.exports = new datafire.Action({
   handler: async (input, context) => {
-    let result = await Promise.all([].map(item => box_authorization.authorize({
-      response_type: "",
-      client_id: "",
-      redirect_uri: "",
-      state: "",
-    }, context)));
-    return result;
+
   },
 });
