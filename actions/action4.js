3be5c4539837393520fce40386c1a88088b6a0c9
DataFire <info@datafire.io>
2020-07-01 12:02:13 -0500

    Edit DataFire project

--- actions/action4.js
+++ actions/action4.js
@@ -1,12 +1,8 @@
 "use strict";
 let datafire = require('datafire');
 
-let dropbox_content = require('@datafire/dropbox_content').actions;
 module.exports = new datafire.Action({
   handler: async (input, context) => {
-    let result = await Promise.all([].map(item => dropbox_content.oauthCallback({
-      code: "",
-    }, context)));
-    return result;
+
   },
 });
