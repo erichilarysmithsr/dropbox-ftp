ed3f75a37f5bfefe5286a51a37604183e52da3e6
DataFire <info@datafire.io>
2020-07-01 23:46:20 -0500

    Edit DataFire project

--- actions/action4.js
+++ actions/action4.js
@@ -1,8 +1,16 @@
 "use strict";
 let datafire = require('datafire');
 
+let azure_mysql = require('@datafire/azure_mysql').actions;
 module.exports = new datafire.Action({
   handler: async (input, context) => {
-
+    let database = await Promise.all([].map(item => azure_mysql.Databases_Get({
+      'api-version': "",
+      subscriptionId: "",
+      resourceGroupName: "",
+      serverName: "",
+      databaseName: "",
+    }, context)));
+    return database;
   },
 });
